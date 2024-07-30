e6dd65e29df7003db787fa189f8fcb5231e8be07
rajaram2920 <rajaramr2920@gmail.com>
2024-07-30 12:33:51 +0530

    leetcode problem sum 14 solution

---
+++ LongestCommon7.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+
+public class LongestCommon7{
+
+    public static void main(String arg[]){
+            
+        String arr[] ={"flower","flow","fligth"};
+        System.out.println(prefixs(arr));
+
+    }
+
+    public static String prefixs(String []s ){
+        
+        String start = s[0];
+        String end = s[s.length-1];
+        Arrays.sort(s);
+        String ans="";
+
+        for(int i =0;i<start.length();i++){
+            if(start.charAt(i)==end.charAt(i)){
+                 ans += start.charAt(i); 
+            }else{
+                break;
+            }
+        }
+
+    return ans;
+
+    }
+
+}
